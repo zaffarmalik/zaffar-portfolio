6fe303bd1b59e1433f0a456bf6490dd3a36b8922
zaffarmalik <zaffarbari007@gmail.com>
2026-07-30 18:27:36 +0500

    change in projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './Projects.css';
 
 // 1. Apne screenshots yahan import karein (Files src/assets/ folder me rakhein):
-import fbImg from './assets/fb-workflow.png';
-import docImg from './assets/doc-workflow.png';
-import groceryImg from './assets/grocery-workflow.png';
+import fbImg from './src/assets/facebook post via buffer.png';
+import docImg from './src/assets/Smart Documents .png';
+import groceryImg from './src/assets/groocery store.png';
 
 export default function Projects() {
   const [selectedProject, setSelectedProject] = useState(null);
